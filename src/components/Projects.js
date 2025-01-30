3207be1b17e6efe70072acc1fb992e6917d0dc70
majdsalameh1211 <majdsalameh9@gmail.com>
2025-01-30 14:20:18 +0200

    2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,12 +6,17 @@ import '../style/Projects.css';
 const Projects = () => {
   const projects = [
     {
-      title: "Project 1",
-      description: "Description of your project.",
-      link: "https://github.com/majdsalameh1211"
+      title: "WhatsApp ChatBot",
+      description: "WhatsApp Chatbot for Client Interaction 🤖💬 This is a WhatsApp automation project that interacts with clients using the Green API. The chatbot manages conversations dynamically, saves them in a JSON file, and can export the data to Google Docs or a CRM system for further processing.",
+      link: "https://github.com/majdsalameh1211/WhatsApp-ChatBot-JS-and-GreenAPI-"
     },
     {
-      title: "Project 2",
+      title: "GoNature – Park Management System",
+      description: "🌿 GoNature is an intelligent park management system designed for seamless visitor reservations and real-time park operations. Built with Java, JavaFX, and SQL, it optimizes park workflow with:\n\nMulti-user authentication (Clients, Guides, Park Managers, Employees)\n Real-time reservation management\n Waiting list system for full bookings\n Automated SMS notifications for approvals\n Data-driven reports for park usage and visitor trends \n\nThe system syncs dynamically between clients and the server, ensuring efficient park management.\n\n",
+      link: "https://github.com/majdsalameh1211/GoNature"
+    },
+    {
+      title: "Project 3",
       description: "Description of your project.",
       link: "https://github.com/majdsalameh1211"
     }
